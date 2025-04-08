8d6fc8e98d840a27976197d97bbfeb1b36da0b34
Andrey <myemail@example.com>
2025-04-08 13:17:45 +0300

    add sift algo and modify parser

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import requests
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 import cv2
 import pathlib
-
+import csv
 
 def detect_circles(image)->bool:
     try:
@@ -17,14 +18,43 @@ def detect_circles(image)->bool:
 
     # Detect circles using HoughCircles
     circles = cv2.HoughCircles(gray_blurred, cv2.HOUGH_GRADIENT, dp=1.2, minDist=90,
-                               param1=160, param2=75, minRadius=35, maxRadius=200)
+                               param1=160, param2=75, minRadius=35, maxRadius=700)
 
     if circles is not None:
         return True
     return False
 
+def parse_page(url, i=0):
+    options = webdriver.chrome.options.Options()
+    options.add_argument('--disable-blink-features=AutomationControlled')
+    # options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--enable-cookies')
 
-def parse_url(url, i=0):
+    service = webdriver.chrome.service.Service(chrome_driver_path='/usr/local/bin/chromedriver')
+
+    driver = webdriver.Chrome(service=service, options=options)
+
+    driver.get(url)
+    ul_element = driver.find_element(By.XPATH, '//*[@id="content"]/div[6]/div/div[2]/ul')
+
+    # Find all <li> elements with class "gallery image" inside the <ul>
+    li_elements = ul_element.find_elements(By.CLASS_NAME, "gallery_image")
+    csv_filename = "cloudy_nights_full/data.csv"
+    with open(csv_filename, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        for index, li in enumerate(li_elements):
+            a_tag = li.find_element(By.TAG_NAME, "a")  # Find the <a> inside <li>
+            link = a_tag.get_attribute("href")  # Get the href value
+            file, size = parse_url(link, index, i)
+            if file is not None:
+                writer.writerow([file, size[0], size[1], link])
+
+
+
+def parse_url(url, index, i=0):
     options = webdriver.chrome.options.Options()
     options.add_argument('--disable-blink-features=AutomationControlled')
     #options.add_argument('--headless')
@@ -36,23 +66,44 @@ def parse_url(url, i=0):
     service = webdriver.chrome.service.Service(chrome_driver_path='/usr/local/bin/chromedriver')
 
     driver = webdriver.Chrome(service=service, options=options)
+
     driver.get(url)
-    image_elements = driver.find_elements(By.TAG_NAME, 'img')
-    for index, image in enumerate(image_elements):
-        src = image.get_attribute('src')
-        if src and src.startswith('http'):
-            responses = requests.get(src)
-            with open(f'image{index}_{i}.png', 'wb') as f:
-                f.write(responses.content)
-            if not detect_circles(f'image{index}_{i}.png'):
-                file = pathlib.Path(f'image{index}_{i}.png')
-                file.unlink(missing_ok=True)
-            print(f"Downloaded {src}")
+
+
+    # Find the <img> inside the <div>
+    answer = [None, None]
+    try:
+        div_element = driver.find_element(By.ID, "theImage")
+        img_element = div_element.find_element(By.TAG_NAME, "img")
+        # Get the image source URL
+        image_url = img_element.get_attribute("src")
+        print("Image URL:", image_url)
+        # Download the image
+        if image_url:
+            file_name = f'cloudy_nights_full/image{index}_{i}.png'
+            response = requests.get(image_url, stream=True)
+            if response.status_code == 200:
+                with open(file_name, "wb") as file:
+                    file.write(response.content)
+                if not detect_circles(file_name):
+                    file = pathlib.Path(file_name)
+                    file.unlink(missing_ok=True)
+                else:
+                    image = cv2.imread(file_name)
+                    height, width = image.shape[:2]
+                    answer = [file_name, [height, width]]
+    except Exception as e:
+        print("Something went wrong:")
+        print(e)
     driver.quit()
+    return answer
 
 
-parse_url('https://www.cloudynights.com/gallery/category/322-lunar/')
+csv_filename = "cloudy_nights_full/data.csv"
+headers = ["name", "height", "width", "url"]
+with open(csv_filename, 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerow(headers)
+parse_page('https://www.cloudynights.com/gallery/category/322-lunar/')
 for i in range(1, 41):
-    parse_url(f'https://www.cloudynights.com/gallery/category/322-lunar/?st={48*i}', i)
-
-
+    parse_page(f'https://www.cloudynights.com/gallery/category/322-lunar/?st={48*i}', i)
---
+++ sift.py
@@ -0,0 +1,69 @@
+import cv2
+import numpy as np
+import glob
+import os
+
+
+
+
+# Load all images from a folder
+image_folder = "/home/andrey/HSE/Course work/Moon_dataset/moons_centerd"
+image_paths = sorted(glob.glob(os.path.join(image_folder, "*.tif")))
+
+# Read all images
+images = [cv2.imread(img) for img in image_paths]
+
+# Convert images to grayscale
+gray_images = [cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) for img in images]
+
+
+sift = cv2.SIFT_create()
+
+
+keypoints_descriptors = [sift.detectAndCompute(img, None) for img in gray_images]
+
+# Matcher
+FLANN_INDEX_KDTREE = 1
+index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+search_params = dict(checks=150)
+flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+# reference
+base_img = images[0]
+base_kp, base_desc = keypoints_descriptors[0]
+
+# Aligning
+aligned_images = [base_img]
+
+for i in range(1, len(images)):
+    kp, desc = keypoints_descriptors[i]
+
+
+    matches = flann.knnMatch(base_desc, desc, k=2)
+
+    #ratio test
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.5 * n.distance:
+            good_matches.append(m)
+
+    if len(good_matches) > 130:
+        src_pts = np.float32([base_kp[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
+
+        # homography
+        H, mask = cv2.findHomography(dst_pts, src_pts, cv2.RANSAC, 5.0)
+
+
+        height, width, _ = base_img.shape
+        aligned = cv2.warpPerspective(images[i], H, (width, height))
+        aligned_images.append(aligned)
+
+
+output_folder = "aligned_images4"
+os.makedirs(output_folder, exist_ok=True)
+
+for i, img in enumerate(aligned_images):
+    cv2.imwrite(os.path.join(output_folder, f"aligned_{i}.jpg"), img)
+
+print(f"Aligned images saved in {output_folder}")
